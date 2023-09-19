7d11fd966b6fcbaa663cbf8fb75c60c3cd4eedd2
Leskiv Andrii <48750279+andrrik@users.noreply.github.com>
2023-09-19 18:05:00 +0300

    Add files via upload

--- DZ_1/app.js
+++ DZ_1/app.js
@@ -2,18 +2,38 @@ const fs = require('node:fs');
 const path = require('node:path');
 
 const directoryPath = path.join(__dirname, 'Boxed_Directory');
+fs.mkdir(directoryPath, (err) => {
+    if (err) {
+      console.error('Error with created folden:', err);
+    } else {
+      console.log('Folder created successfully.');
+    }
+});
+
 let countBox = 5;
 for(let i = 1; i <= countBox; i++ ){
     let folderName = `Directory_${i}`;
     let folderPath = path.join(directoryPath, folderName);
-    fs.mkdir(folderPath, (err) => {})
+    fs.mkdir(folderPath, (err) => {
+        if (err) {
+            console.error('Error creating folders:', err);
+        } else {
+            console.log('Folders created successfully.');
+        }
+    })
 }
 
 for(let i = 1; i <= countBox; i++ ){
     let fileName = `TextFile_${i}.txt`;
     let folderPath = path.join(directoryPath, fileName);
     let fileContent = `Це вміст файлу ${fileName}`;
-    fs.writeFile(folderPath, fileContent, (err) => {})
+    fs.writeFile(folderPath, fileContent, (err) => {
+        if (err) {
+            console.error('Error creating files:', err);
+        } else {
+            console.log('Files created successfully.');
+        }
+    })
 }
 fs.readdir(directoryPath, (err, files) => {
     files.forEach((file) => {
